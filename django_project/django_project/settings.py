4365cff4038fe19d17bcffc7a9fce93bb55edff0
Your Name <nirajkumar09160@gmail.com>
2026-04-23 18:16:45 +0530

    wrong des

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
-    "products",         # for products app
+
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,7 @@ ROOT_URLCONF = "django_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
