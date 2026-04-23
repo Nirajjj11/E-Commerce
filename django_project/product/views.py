aba1acd4948eac31318245f0781d9861d407dff3
Your Name <nirajkumar09160@gmail.com>
2026-04-24 03:14:55 +0530

    some templates made

--- django_project/product/views.py
+++ django_project/product/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DeleteView, DetailView, CreateView,UpdateView
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
+from .models import Product, Order, OrderItems, OrderItemTracking
 # Create your views here.
+class ProductListView(ListView):
+      model = Product
+      template_name = 'product/product_list.html'
+      context_object_name = "products"
+      
+      def get_queryset(self):
+            return Product.objects.all().order_by('-created_at')
