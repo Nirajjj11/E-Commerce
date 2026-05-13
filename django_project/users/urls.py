40d04c727eab0e1596e6f51805faad13b6cfaf16
Your Name <nirajkumar09160@gmail.com>
2026-05-13 11:43:47 +0530

    Product wish list and acc to cart is showing

--- django_project/users/urls.py
+++ django_project/users/urls.py
@@ -4,13 +4,9 @@ from .views import *
 urlpatterns = [
 
       path("", UserDashboardView.as_view(), name="user_dashboard"),
-
-      # path("wishlist/", WishlistView.as_view(), name="wishlist"),
-
-      # path("cart/", CartView.as_view(), name="cart"),
-
-      # path("orders/", OrderHistoryView.as_view(), name="order_history"),
-
-      # path("tracking/<int:pk>/", OrderTrackingView.as_view(), name="tracking"),
+      path("wishlist/", WishListView.as_view(), name="wishlist"),
+      path("wishlist/add/<int:pk>/", AddToWishlistView.as_view(),name="add_to_wishlist"),
+      path("wishlist/remove/<int:pk>/",RemoveFromWishlistView.as_view(),name="remove_from_wishlist"),
+      path("cart/add/<int:pk>/", AddToCartView.as_view(),name="add_to_cart"),
 
 ]
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,5 +1,45 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect, get_object_or_404
+from .models import WishList, Cart
+from product.models import Product
+
+
 
 class UserDashboardView(LoginRequiredMixin, TemplateView):
       template_name = "dashboard.html"
+
+class WishListView(LoginRequiredMixin, ListView):
+      model = WishList
+      template_name = "wishlist.html"
+      context_object_name = "wishlist_items"
+
+      def get_queryset(self):
+            return WishList.objects.filter(
+                  user=self.request.user
+            ).select_related("product").order_by("-added_at")
+      
+# Add to wishlist
+class AddToWishlistView(LoginRequiredMixin, View):
+      def get(self, request,pk):
+            product = get_object_or_404(Product, pk=pk)
+            wishlist_item , created = WishList.objects.get_or_create(user = request.user, product = product)
+            return redirect("wishlist")
+      
+# REMOVE FROM WISHLIST
+class RemoveFromWishlistView(LoginRequiredMixin, View):
+      def get(self, request, pk):
+            wishlist_item = get_object_or_404( WishList,pk=pk, user=request.user
+            )
+            wishlist_item.delete()
+            return redirect("wishlist")
+
+# ADD TO CART
+class AddToCartView(LoginRequiredMixin, View):
+      def get(self, request, pk):
+            product = get_object_or_404(Product, pk=pk)
+            cart_item, created = Cart.objects.get_or_create( user=request.user, product=product)
+            if not created:
+                  cart_item.quantity += 1
+                  cart_item.save()
+            return redirect("cart")
