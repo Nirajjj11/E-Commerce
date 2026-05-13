06d3737133019a2424522b9a582c448a5454ab8c
Your Name <nirajkumar09160@gmail.com>
2026-05-13 13:06:11 +0530

    Checkout

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -105,50 +105,62 @@ class ApplyCouponView(LoginRequiredMixin, View):
             return redirect("cart")
 
 # CHECKOUT VIEWS
-class CheckoutView(LoginRequiredMixin, View):
+class CheckoutView(LoginRequiredMixin, TemplateView):
       template_name = "checkout.html"
-      def get(self, request):
-            cart_items = Cart.objects.filter(user=request.user)
-            subtotal = sum(item.product.price * item.quantity for item in cart_items )
 
-            # Charges
+      def get_context_data(self, **kwargs):
+
+            context = super().get_context_data(**kwargs)
+
+            cart_items = Cart.objects.filter(
+                  user=self.request.user
+            ).select_related("product")
+
+            subtotal = sum(item.product.actual_price * item.quantity for item in cart_items)
+
+            # Delivery Logic
             delivery_charge = 0 if subtotal > 500 else 40
+
+            # Platform Fee
             platform_fee = 10
 
-            # Membership logic
-            is_member = False
+            # Membership
+            is_member = self.request.session.get("is_prime", False)
 
             if is_member:
                   delivery_charge = 0
                   platform_fee = 0
 
+            # Discount
             disc_amount = 0
 
+            # Final Total
             total = (
                   subtotal
                   + delivery_charge
                   + platform_fee
                   - disc_amount
             )
+            context["items"] = cart_items
+            context["subtotal"] = subtotal
+            context["delivery_charge"] = delivery_charge
+            context["platform_fee"] = platform_fee
+            context["disc_amount"] = disc_amount
+            context["total"] = total
+            context["is_member"] = is_member
 
-            context = {
-                  "items": cart_items,
-                  "subtotal": subtotal,
-                  "delivery_charge": delivery_charge,
-                  "platform_fee": platform_fee,
-                  "disc_amount": disc_amount,
-                  "total": total,
-                  "is_member": is_member,
-            }
+            return context
 
-            return render(
-                  request,
-                  self.template_name,
-                  context
-            )
-
-      def post(self, request):
+      def post(self, request, *args, **kwargs):
             payment_method = request.POST.get("payment_method")
-            print(payment_method)
-            # order creation logic here
-            return render(request,self.template_name)
+            cart_items = Cart.objects.filter(user=request.user)
+            if not cart_items.exists():
+                  messages.error(request,"Your cart is empty.")
+                  return redirect("cart")
+
+            # Create Order Logic Here
+            messages.success(request,f"Order placed successfully using {payment_method}")
+            
+            # Clear cart
+            cart_items.delete()
+            return redirect("dashboard")
